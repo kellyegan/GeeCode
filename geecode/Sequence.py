a95e7c0d3f2f347150cdf749ba2bb200088205e3
Kelly Egan <kelly.egan@gmail.com>
2023-01-03 10:57:30 -0500

    Add cmd method to Sequence

--- geecode/Sequence.py
+++ geecode/Sequence.py
@@ -7,3 +7,8 @@ class Sequence:
     def cmd(self, command_code, **parameters):
         command = Command(command_code, **parameters)
         self._commands.append(command)
+
+    def generate(self):
+        gcode = "\n".join([c.generate().strip() for c in self._commands])
+        print(gcode)
+        return gcode
--- tests/TestSequence.py
+++ tests/TestSequence.py
@@ -12,5 +12,20 @@ class TestSequence(unittest.TestCase):
         s.cmd("G1", x=15, y=16, e=0.05)
 
         added_command = s._commands[0]
+        self.assertEqual(len(s._commands), 1)
         self.assertIsInstance(added_command, Command)
         self.assertEqual(added_command, c)
+
+        # Add second command
+        s.cmd("G1", x=25, y=36, e=0.05)
+        self.assertEqual(len(s._commands), 2)
+
+    def test_generate_gcode(self):
+        s = Sequence()
+        s.cmd("M73", p=0, r=0, comment="set print progress")
+        s.cmd("M205", x=10, y=10, z=0.2, e=4.5, comment="sets the jerk limits, mm/sec")
+        s.cmd("M207", comment="Fans off")
+        expected_output = "M73 P0 R0                          ; set print progress\n" \
+                          "M205 X10 Y10 Z0.2 E4.5             ; sets the jerk limits, mm/sec\n" \
+                          "M207                               ; Fans off"
+        self.assertEqual(expected_output, s.generate())
