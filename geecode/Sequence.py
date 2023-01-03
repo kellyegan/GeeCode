3103b65c7b578340802dcdb5af6d686044a8e925
Kelly Egan <kelly.egan@gmail.com>
2023-01-03 11:11:26 -0500

    Add parameter for including comments to Sequence.

--- geecode/Sequence.py
+++ geecode/Sequence.py
@@ -9,6 +9,6 @@ class Sequence:
         command = Command(command_code, **parameters)
         self._commands.append(command)
 
-    def generate(self, comment_indent=35):
-        gcode = "\n".join([c.generate(comment_indent=comment_indent).strip() for c in self._commands])
+    def generate(self, include_comments=True, comment_indent=35):
+        gcode = "\n".join([c.generate(comment_indent=comment_indent, include_comments=include_comments).strip() for c in self._commands])
         return gcode
--- tests/TestSequence.py
+++ tests/TestSequence.py
@@ -5,6 +5,12 @@ from geecode import Sequence
 
 class TestSequence(unittest.TestCase):
 
+    def setUp(self):
+        self.s = Sequence()
+        self.s.cmd("M73", p=0, r=0, comment="set print progress")
+        self.s.cmd("M205", x=10, y=10, z=0.2, e=4.5, comment="sets the jerk limits, mm/sec")
+        self.s.cmd("M207", comment="Fans off")
+
     def test_add_command(self):
         """Add a command to the sequence"""
         s = Sequence()
@@ -22,22 +28,21 @@ class TestSequence(unittest.TestCase):
 
     def test_generate_gcode(self):
         """Test if generates correct gcode"""
-        s = Sequence()
-        s.cmd("M73", p=0, r=0, comment="set print progress")
-        s.cmd("M205", x=10, y=10, z=0.2, e=4.5, comment="sets the jerk limits, mm/sec")
-        s.cmd("M207", comment="Fans off")
         expected_output = "M73 P0 R0                          ; set print progress\n" \
                           "M205 X10 Y10 Z0.2 E4.5             ; sets the jerk limits, mm/sec\n" \
                           "M207                               ; Fans off"
-        self.assertEqual(expected_output, s.generate())
+        self.assertEqual(expected_output, self.s.generate())
 
     def test_custom_indent(self):
         """Test if sequence applies a custom comment indentation"""
-        s = Sequence()
-        s.cmd("M73", p=0, r=0, comment="set print progress")
-        s.cmd("M205", x=10, y=10, z=0.2, e=4.5, comment="sets the jerk limits, mm/sec")
-        s.cmd("M207", comment="Fans off")
         expected_output = "M73 P0 R0                ; set print progress\n" \
                           "M205 X10 Y10 Z0.2 E4.5   ; sets the jerk limits, mm/sec\n" \
                           "M207                     ; Fans off"
-        self.assertEqual(expected_output, s.generate(comment_indent=25))
+        self.assertEqual(expected_output, self.s.generate(comment_indent=25))
+
+    def test_no_comment(self):
+        """Test if sequence applies a custom comment indentation"""
+        expected_output = "M73 P0 R0\n" \
+                          "M205 X10 Y10 Z0.2 E4.5\n" \
+                          "M207"
+        self.assertEqual(expected_output, self.s.generate(include_comments=False))
