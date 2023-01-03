7e643b8181897c2d248a0d48e0c11c6ca620b29b
Kelly Egan <kelly.egan@gmail.com>
2023-01-03 18:13:58 -0500

    Add docstrings to Sequence class

--- geecode/Sequence.py
+++ geecode/Sequence.py
@@ -6,12 +6,30 @@ class Sequence:
         self._commands = []
 
     def cmd(self, command_code, **parameters):
+        """
+        Add a command to the sequence
+        :param command_code: G-code command to execute
+        :param parameters: key-value parameters representing g-code commands parameters
+        :return:
+        """
         command = Command(command_code, **parameters)
         self._commands.append(command.generate)
 
     def sub(self, sub_sequence):
+        """
+        Add a sub-sequence to this sequence
+        :param Sequence sub_sequence:
+        :return:
+        """
         self._commands.append(sub_sequence.generate)
 
     def generate(self, include_comments=True, comment_indent=35, **parameters):
+        """
+        Generate gcode for this sequence
+        :param include_comments: Boolean to sets whether comments are included
+        :param comment_indent: Number of spaces the comment is justified from command.
+        :param parameters: Any parameters for this command
+        :return: gcode string
+        """
         gcode = "\n".join([c(comment_indent=comment_indent, include_comments=include_comments) for c in self._commands])
         return gcode
